a0fee5099e1ee2e279d8f27f747fe2cb11c30560
Viktor <simpat007@yandex.ru>
2021-05-11 11:01:18 +0300

    fix input sign-in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -53,7 +53,7 @@ const SignIn = () => {
         <label className="sign-in__form-label">
           <span className="sign-in__title-form">Пароль</span>
           <input
-            className="sign-up__checkbox-text"
+            className="sign-in__checkbox-text"
             type="password"
             ref={register({ required: true, minLength: 8, maxLength: 40 })}
             name="password"
